8a2d115c2fca6a4944dd237d77c0e5a0321b1633
Domek0077 <dominik.trzopek92@gmail.com>
2026-08-30 12:48:32 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-
+export default {
+  async fetch(request, env) {
+    return new Response(
+      "🔥 Pokémon TCG Monitor działa!",
+      {
+        headers: {
+          "content-type": "text/plain; charset=UTF-8"
+        }
+      }
+    );
+  }
+};
